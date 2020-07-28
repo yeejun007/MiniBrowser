fbcebc5172608b0225fffdf3dc3e28771fb3c1c8
yeejun007 <dlwns007@gmail.com>
2020-07-28 20:02:52 +0900

    Initial Commit

---
+++ MiniBrowser/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MiniBrowser
+//
+//  Created by 송이준 on 28/07/2020.
+//  Copyright © 2020 yeejun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MiniBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MiniBrowser
+//
+//  Created by 송이준 on 28/07/2020.
+//  Copyright © 2020 yeejun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
