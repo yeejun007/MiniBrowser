ec235114db0ab491d00aabaacf6d85abd09b9454
yeejun007 <dlwns007@gmail.com>
2020-07-30 15:48:54 +0900

    added property on ViewController.h

--- MiniBrowser/ViewController.h
+++ MiniBrowser/ViewController.h
@@ -10,6 +10,10 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UISegmentedControl *bookmarkSegmentControll;
+@property (weak, nonatomic) IBOutlet UITextField *urlTextField;
+@property (weak, nonatomic) IBOutlet UIWebView *mainWebView;
+
 
 @end
 
