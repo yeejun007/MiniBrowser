1a0cc3c38d47b535d1a70f10d6c534d6c94fcb6f
yeejun007 <dlwns007@gmail.com>
2020-07-31 18:03:22 +0900

    compelted UIActivityIndicatorView

--- MiniBrowser/ViewController.h
+++ MiniBrowser/ViewController.h
@@ -9,12 +9,13 @@
 #import <UIKit/UIKit.h>
 
 // 애플이 준비해둔 프로토콜을 delegate를 통해 사용하기 위해 < > 괄호 안에 명시해준다
-@interface ViewController : UIViewController<UITextFieldDelegate>
+@interface ViewController : UIViewController<UITextFieldDelegate, UIWebViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UISegmentedControl *bookmarkSegmentControl;
 @property (weak, nonatomic) IBOutlet UITextField *urlTextField;
 @property (weak, nonatomic) IBOutlet UIWebView *mainWebView;
 - (IBAction)bookmarkAction:(id)sender;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicatorView;
 
 
 @end
--- MiniBrowser/ViewController.m
+++ MiniBrowser/ViewController.m
@@ -13,10 +13,15 @@
 @end
 
 @implementation ViewController
-@synthesize mainWebView, urlTextField, bookmarkSegmentControl;
+@synthesize mainWebView, urlTextField, bookmarkSegmentControl, activityIndicatorView;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    // activityIndicatorView를 요청상황을 추적해서 로딩이 완료되었는지 여부에 따라 사용하려면
+    // UIWebViewDelegate를 사용해야한다
+    // UIWebViewDelegate를 통해 WebView의 로딩완료 여부를 체크할 수 있다 
+//    [activityIndicatorView startAnimating];
+    
     // http로 접속이 안되는경우 보안때문임. https를 사용해야하는 웹인 경우 안될 수가 있다.
     // 이떄는 info.plist로 가서 App transfer security setting 추가
     // 그 아래에 다시 Allow Arbitrary Loads 추가하고 그 속성에 YES라고 수정하면 됨
@@ -25,6 +30,7 @@
     [mainWebView loadRequest: [NSURLRequest requestWithURL: [NSURL URLWithString: urlString]]];
     urlTextField.text = urlString;
     
+//    [activityIndicatorView stopAnimating];
     
 }
 
@@ -33,6 +39,7 @@
 // url을 입력하고 엔터(return 키보드 키)를 치면 해당 url로 이동하도록 하면된다
 // storyboard에서 urlTextField 마우스 오른 클릭하고 delegate를 앱 자기 자신에게 이어준다
 -(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    
     NSString *urlString = urlTextField.text;
     
     // 만약 주소입력칸에 http:// 가 빠져있다면 이거 붙여주는 로직 작성하기
@@ -45,11 +52,13 @@
     [mainWebView loadRequest: [NSURLRequest requestWithURL: [NSURL URLWithString: urlString]]];
     // 포커스 비활성화 시켜서 키보드기 내려가도록 한다
     [textField resignFirstResponder];
+    
     return YES;
 }
 
 
 - (IBAction)bookmarkAction:(id)sender {
+    
     // 문서보면 UISegmentedControl 클래스에서 해당 인덱스 가져오는 메소드가 있는거 볼 수 있음
     // 그리고 지정된 인덱스 segment의 title을 가져오는 메소드도 있다
     // 이 인덱스 숫자는 primitive타입이므로 변수 선언할 때 * 이거 붙이면 안됨
@@ -58,6 +67,17 @@
     NSString *bookmarkURL = [bookmarkSegmentControl titleForSegmentAtIndex: bookmarkSegmentControl.selectedSegmentIndex];
     NSString *urlString = [[NSString alloc] initWithFormat:@"http://www.%@.com", bookmarkURL];
     [mainWebView loadRequest: [NSURLRequest requestWithURL: [NSURL URLWithString: urlString]]];
+    
+}
+
+
+// storyboard에서 webview의 delegate를 앱 자기자신에게 연결시키기
+-(void)webViewDidStartLoad:(UIWebView *)webView {
+    [activityIndicatorView startAnimating];
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView {
+    [activityIndicatorView stopAnimating];
 }
 
 @end
